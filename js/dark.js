1cbc9f01e93e8dd27165d40d4c1a2577673c55d5
John8414 <jmnguyen276@gmail.com>
2022-05-02 15:26:44 +0700

    update hover

--- js/dark.js
+++ js/dark.js
@@ -1,3 +1,9 @@
+// turn dark mode
 document.getElementById("switchMode").onclick = function () {
 document.getElementById("modeSwitchDiv").classList.add("darkMode");
 }
+
+// turn light mode
+document.getElementById("switchLight").onclick = function () {
+    document.getElementById("modeSwitchDiv").classList.remove("darkMode");
+    }
